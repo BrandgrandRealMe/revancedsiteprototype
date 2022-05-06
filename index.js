daacec4f338b3c473d8fbf23a24a9f4c9b5e2b91
BrandgrandReal <47084010+BrandgrandRealMe@users.noreply.github.com>
2022-05-06 21:56:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const fs = require('fs');
+app.use(express.static("public"));
+// Home
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/pages/home/home.html");
+});
+app.get("/file/home.css", (request, response) => {
+  response.sendFile(__dirname + "/pages/home/home.css");
+});
+app.get("/file/home.js", (request, response) => {
+  response.sendFile(__dirname + "/pages/home/home.js");
+});
+
+// license
+app.get("/license", (request, response) => {
+  response.sendFile(__dirname + "/pages/license/license.html");
+});
+app.get("/file/license.css", (request, response) => {
+  response.sendFile(__dirname + "/pages/license/license.css");
+});
+
+// File Stuff
+app.get('/favicon/:fname', (req, res) => {
+ if (!fs.readFileSync('./favicon/' + req.params.fname)) r404(res)
+ else
+  res.sendFile(__dirname + '/favicon/' + req.params.fname);
+});
+app.get('/public/:fname', (req, res) => {
+ if (!fs.readFileSync('./public/' + req.params.fname)) r404(res)
+ else
+  res.sendFile(__dirname + '/public/' + req.params.fname);
+});
+app.get('/file/logo/:fname', (req, res) => {
+ if (!fs.readFileSync('./logos/' + req.params.fname)) r404(res)
+ else
+  res.sendFile(__dirname + '/logos/' + req.params.fname);
+});
+app.get('/fonts/:fname', (req, res) => {
+ if (!fs.readFileSync('./fonts/' + req.params.fname)) r404(res)
+ else
+  res.sendFile(__dirname + '/fonts/' + req.params.fname);
+});
+
+
+
+// 404
+app.get("/file/404.css", (request, response) => {
+  response.sendFile(__dirname + "/pages/404/404.css");
+});
+app.get("/file/404.js", (request, response) => {
+  response.sendFile(__dirname + "/pages/404/404.js");
+});
+app.get('*', function(req, res){
+  res.sendFile(__dirname + '/pages/404/404.html');
+});
+
+const listener = app.listen(process.env.PORT, () => {
+
+});
